b5333ed42f5b3eac8de8e15fadb841e79ea70aee
MD <MD@qq.com>
2025-05-16 09:33:14 +0800

    添加登录界面并成功的初始化版本

--- plant_diseases/plant_app/views.py
+++ plant_diseases/plant_app/views.py
@@ -32,6 +32,7 @@ def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
+
         return render(request, 'plant_app/index.html')
         #user = authenticate(request, username=username, password=password)
         #if user is not None:
